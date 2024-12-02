1255264f229399c30cd08d86d34daae97de21fef
hakhddhujsjdyw <manafalthaf01@gmail.com>
2024-12-02 09:01:23 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -189,8 +189,10 @@ Name - {}
     
 {file_caption}
 
-🔗 𝗠𝗔𝗜𝗡 𝗖𝗛𝗔𝗡𝗡𝗘𝗟 𝗟𝗜𝗡𝗞 ☞ https://t.me/+uA5gEKm8WXk1ZTll
-https://t.me/+uA5gEKm8WXk1ZTll
+🔗 𝗠𝗔𝗜𝗡 𝗖𝗛𝗔𝗡𝗡𝗘𝗟 𝗟𝗜𝗡𝗞 ☞ 
+t.me/MCUupdatesLINKS
+t.me/+uA5gEKm8WXk1ZTll
 
-🔗 𝙈𝙤𝙫𝙞𝙚 𝙍𝙚𝙦𝙪𝙚𝙨𝙩 𝙂𝙧𝙤𝙪𝙥 ☞ https://t.me/+3DmUxFC-wj05ODE1
-https://t.me/+3DmUxFC-wj05ODE1</b>"""
+🔗 𝙈𝙤𝙫𝙞𝙚 𝙍𝙚𝙦𝙪𝙚𝙨𝙩 𝙂𝙧𝙤𝙪𝙥 ☞ 
+t.me/+eVBVOyp9PRk3YmFl
+t.me/+eVBVOyp9PRk3YmFl</b>"""
