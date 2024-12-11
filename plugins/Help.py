b9f7654aafc8a08d09e081a102c86ca5e330a66a
hakhddhujsjdyw <manafalthaf01@gmail.com>
2024-12-12 00:12:29 +0530

    Create Help.py

---
+++ plugins/Help.py
@@ -0,0 +1,6 @@
+@Client.on_message(filters.command("link"))
+async def generate_link(client, message):
+    command_text = message.text.split(maxsplit=1)
+    if len(command_text) < 2:
+        await message.reply("Please provide the name for the movie! Example: `/link game of thrones`")
+        return
