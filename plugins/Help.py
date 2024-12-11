83fde0016d3be0302349356223a40aa3510ebf47
hakhddhujsjdyw <manafalthaf01@gmail.com>
2024-12-12 00:12:58 +0530

    Update Help.py

--- plugins/Help.py
+++ plugins/Help.py
@@ -1,3 +1,6 @@
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+
 @Client.on_message(filters.command("link"))
 async def generate_link(client, message):
     command_text = message.text.split(maxsplit=1)
