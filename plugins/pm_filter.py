14c0ac88e80469abc7e016189df99f1451bd9b19
hakhddhujsjdyw <manafalthaf01@gmail.com>
2024-12-17 00:17:13 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -792,14 +792,17 @@ async def auto_filter(client, msg, spoll=False):
         offset = 0
     
     if offset == 0:
-        
+        btn.append(
+            [InlineKeyboardButton(text="1/1", callback_data="pages")]
+        )
+    else:
         key = f"{message.chat.id}-{message.id}"
         BUTTONS[key] = search
         req = message.from_user.id if message.from_user else 0
         btn.append(
             [InlineKeyboardButton(text=f"1/{math.ceil(int(total_results) / 10)}", callback_data="pages"),
             InlineKeyboardButton(text="Nᴇxᴛ ⤷", callback_data=f"next_{req}_{key}_{offset}")]
-       )
+        )
     
     imdb = await get_poster(search, file=(files[0]).file_name) if settings["imdb"] else None
     TEMPLATE = settings['template']
