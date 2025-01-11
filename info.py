a2d98129b1a90b3dc326e2f6fad67e447f3a824c
hakhddhujsjdyw <manafalthaf01@gmail.com>
2025-01-11 22:47:00 +0530

    Update info.py

--- info.py
+++ info.py
@@ -16,7 +16,7 @@ def is_enabled(value, default):
 SESSION = environ.get('SESSION', 'autodelete')
 API_ID = int(environ.get("API_ID", "19071424"))
 API_HASH = environ.get("API_HASH", "c4b3e298cc50fd4cc563ae75ee882948")
-BOT_TOKEN = environ.get("BOT_TOKEN", "7627787641:AAFqphIASFG9jA-yLgGEqlT5wLsCegJnV5w")
+BOT_TOKEN = environ.get("BOT_TOKEN", "7627787641:AAFdjGR2ydJ8cg4gPH5ynC3S0bYVDSRcEAc")
 
 # Bot settings
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
