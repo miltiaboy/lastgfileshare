904b75c3978355fd932e701b497b2b5d19e9cec9
hakhddhujsjdyw <manafalthaf01@gmail.com>
2025-01-23 08:58:22 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -3,155 +3,10 @@ class script(object):
 
 Here You Can Request Movie's, Just Sent <a href='https://t.me/mcu_Mammootty_bot'>Movie Name</a> With Proper <a href='https://www.google.com/'>Google</a> Spelling..!!
 
-Fᴏʀ Mᴏʀᴇ Dᴇᴛᴀɪʟꜱ Cʟɪᴄᴋ /help</b>"""
-    
-    HELP_TXT = """<b>𝖧𝖾𝗒 {}, 𝖨𝖺𝗆 𝖧𝖺𝗉𝗉𝗒 🖤 𝖳𝗈 𝖧𝖺𝗏𝖾 𝖸𝗈𝗎
-    
-𝖶𝗂𝗍𝗁 𝖳𝗁𝗂𝗌 𝖧𝖾𝗅𝗉 𝖸𝗈𝗎 𝖶𝗂𝗅𝗅 𝖴𝗇𝖽𝖾𝗋𝗌𝗍𝖺𝗇𝖽 𝖧𝗈𝗐 𝖳𝗈 𝖴𝗌𝖾 𝖳𝗁𝗂𝗌 𝖡𝗈𝗍🏌️
-    
-𝖢𝗁𝖾𝖼𝗄 𝖡𝖾𝗅𝗈𝗐 𝖧𝖾𝗅𝗉 𝖥𝗎𝗇𝖼𝗍𝗂𝗈𝗇𝗌🤍 </b>"""
-    
-    ABOUT_TXT = """<b>𝖧𝖾𝗒 𝖡𝗋𝗈 𝖨𝖺𝗆 𝖧𝖺𝗉𝗉𝗒 🖤 𝖳𝗈 𝖧𝖺𝗏𝖾 𝖸𝗈𝗎</b>
+Fᴏʀ Mᴏʀᴇ Dᴇᴛᴀɪʟꜱ Cʟɪᴄᴋ /help
 
-𝖨𝖺𝗆 𝖯𝗈𝗐𝖾𝗋𝖥𝗎𝗅𝗅 𝖠𝗎𝗍𝗈 𝖥𝗂𝗅𝗍𝖾𝗋 + 𝖬𝗈𝗏𝗂𝖾 𝖲𝖾𝖺𝗋𝖼𝗁 + 𝖬𝖺𝗇𝗎𝖺𝗅 𝖥𝗂𝗅𝗍𝖾𝗋 𝖡𝗈𝗍 ⚙
+Cᴏɴᴛᴀᴄᴛ Bᴏᴛ Dᴇᴠᴇʟᴏᴘᴇʀ (Oʀ) Rᴇᴘᴏʀᴛ Bᴜɢꜱ..!! 👉 @MCU_ADMIN_V1_BOT</b>"""
 
-𝖧𝗈𝗌𝗍𝖾𝖽 𝖡𝗒 @MCU_ADMIN_V1_BOT 𝖣𝖾𝗏𝖾𝗅𝗈𝗉𝗂𝗇𝗀 𝖬𝖾 𝗂𝗇 𝖵𝖯𝖲. 𝖸𝗈𝗎 𝖢𝖺𝗇 𝖩𝗈𝗂𝗇 𝖠𝗇𝖽 𝖱𝖾𝗊𝗎𝖾𝗌𝗍 𝗂𝗇 𝖬𝗈𝗏𝗂𝖾𝗌 𝖦𝗋𝗈𝗎𝗉 @MCUupdatesLINKS
-"""
-
-    SOURCE_TXT = """<b>കൊട്ക്ക്ണില്ല്യാ......</b>"""
-    
-    MCAHU_TXT = """The new command features are listed below. More features coming soon
-    
-/start - Welcome🤗 (Help , About and others)
-    
-/setchat1 - 1st force Sub Channel ID
-
-/setchat2 - 2nd Force Sub Channel ID
-
-/totalreq - Total Req in 2 Fsub Channels
-
-/purge_one - Delete Requests (1st fsub)
-
-/purge_two - Delete Requests (2nd fsub) 
-
-/stats - Check Stats
-
-/ping - Bot Speed , Cpu etc
-
-/restart - Restart ✅
-
-/logs - bot logs txt"""
-    BUTER_TXT = """<b>⍟ 𝗠𝘆 𝗡𝗮𝗺𝗲 : <a href=https://t.me/{}>{}</a>
-⍟ 𝗠𝘆 𝗢𝘄𝗻𝗲𝗿 : @MCU_ADMIN_V1_BOT
-⍟ 𝗠𝘆 𝗦𝗲𝗿𝘃𝗲𝗿 : Digital.Ocean
-
-✪ സിനിമകൾ ഇഷ്ടപ്പെടുന്നവർക്കും സിനിമ ഡൗൺലോഡ് ചെയ്യുന്നവർക്കും വേണ്ടിയുള്ള മാത്രം ബോട്ട് ആണിത് 🤩
-
-✫ 𝖸𝗈𝗎 𝖢𝖺𝗇 𝖲𝖾𝗇𝖽 𝖡𝗈𝗍 𝖠𝖽𝗆𝗂𝗇 𝖬𝖾𝗌𝗌𝖺𝗀𝖾 𝖥𝗈𝗋 𝖴𝗉𝗅𝗈𝖺𝖽𝗂𝗇𝗀 𝖬𝗈𝗏𝗂𝖾𝗌, 𝖯𝖺𝗂𝖽 𝖯𝗋𝗈𝗆𝗈𝗍𝗂𝗈𝗇 𝖾𝗍𝖼</b>"""
-    WHYER_TXT = """<b>✰ സിനിമയെ ഇഷ്ടപ്പെടുന്നവർക്കായി നിങ്ങൾക്ക് ഞങ്ങളുടെ സിനിമ കൂട്ടായ്മയിലേക്ക് (സിനിമ പ്രേമി) എല്ലാവരും ജോയിൻ ചെയ്യുക🤍
-    
-◍ <a href=https://t.me/+2FCs53JjHZo0MmZl>𝐂𝐨𝐦𝐦𝐮𝐧𝐢𝐭𝐲 - 𝐂𝐥𝐢𝐜𝐤 𝐇𝐞𝐫𝐞</a> ||(100𝗄 𝖣𝗋𝖾𝖺𝗆 🤗, 𝗒𝗈𝗎 𝗌𝗁𝗈𝗎𝗅𝖽 𝗌𝗎𝗉𝗉𝗈𝗋𝗍!🤎)||
-    
-➪ 𝖸𝗈𝗎 𝖼𝖺𝗇 𝖺𝗅𝗌𝗈 𝗎𝗌𝖾 𝗂𝗍 𝗂𝖿 𝗒𝗈𝗎 𝖺𝖽𝖽 𝗆𝖾 𝗍𝗈 𝗒𝗈𝗎𝗋 𝗀𝗋𝗈𝗎𝗉✨
-
-◍ <a href=https://t.me/mcu_Mammootty_bot?startgroup=true>𝐀𝐝𝐝 𝐌𝐞 𝐓𝐨 𝐘𝐨𝐮𝐫 𝐆𝐫𝐨𝐮𝐩</a>
-    
-ᴥ︎︎︎ 𝖳𝗈 𝗀𝖾𝗍 𝗆𝗈𝗏𝗂𝖾𝗌, 𝗒𝗈𝗎 𝖼𝖺𝗇 𝗋𝖾𝗊𝗎𝖾𝗌𝗍 𝗍𝗁𝖾 𝗀𝗋𝗈𝗎𝗉 𝗎𝗌𝗂𝗇𝗀 𝗍𝗁𝖾 𝖻𝗎𝗍𝗍𝗈𝗇 𝖻𝖾𝗅𝗈𝗐, 𝖿𝗈𝗋 𝗈𝗍𝗁𝖾𝗋 𝗍𝗁𝗂𝗇𝗀𝗌, 𝗒𝗈𝗎 𝖼𝖺𝗇 𝗌𝖾𝗇𝖽 𝖺 𝗆𝖾𝗌𝗌𝖺𝗀𝖾 𝗍𝗈 𝗍𝗁𝖾 𝖻𝗈𝗍 𝗀𝗋𝗈𝗎𝗉 𝖺𝖽𝗆𝗂𝗇.</b>"""
-    EXSERI_TXT = """Help: <b>Extra Modules</b>
-
-<b>NOTE:</b>
-these are the extra features of Mr.Bean
-
-<b>Commands and Usage:</b>
-• /id - <code>get id of a specified user.</code>
-• /info  - <code>get information about a user.</code>
-• /imdb  - <code>get the film information from IMDb source.</code>
-• /search  - <code>get the film information from various sources.</code>"""
-    MANUELFILTER_TXT = """Help: <b>Filters</b>
-
-- Filter is the feature were users can set automated replies for a particular keyword and EvaMaria will respond whenever a keyword is found the message
-
-<b>NOTE:</b>
-1. eva maria should have admin privillage.
-2. only admins can add filters in a chat.
-3. alert buttons have a limit of 64 characters.
-
-<b>Commands and Usage:</b>
-• /filter - <code>add a filter in chat</code>
-• /filters - <code>list all the filters of a chat</code>
-• /del - <code>delete a specific filter in chat</code>
-• /delall - <code>delete the whole filters in a chat (chat owner only)</code>"""
-    SETIN_TXT = """Settings is most important features in this Bot
-You can easily customise this Bot for your group
-
-Note
-1. Only group admins can use this command and change settings
-2. It works only when you already connected your group
-
-Command and uses
-/settings - change settings as your wish"
-"""
-    BUTTON_TXT = """Help: <b>Buttons</b>
-
-- Eva Maria Supports both url and alert inline buttons.
-
-<b>NOTE:</b>
-1. Telegram will not allows you to send buttons without any content, so content is mandatory.
-2. Eva Maria supports buttons with any telegram media type.
-3. Buttons should be properly parsed as markdown format
-
-<b>URL buttons:</b>
-<code>[Button Text](buttonurl:https://t.me/EvaMariaBot)</code>
-
-<b>Alert buttons:</b>
-<code>[Button Text](buttonalert:This is an alert message)</code>"""
-    AUTOFILTER_TXT = """Help: <b>Auto Filter</b>
-
-<b>NOTE:</b>
-1. Make me the admin of your channel if it's private.
-2. make sure that your channel does not contains camrips, porn and fake files.
-3. Forward the last message to me with quotes.
- I'll add all the files in that channel to my db."""
-    CONNECTION_TXT = """Help: <b>Connections</b>
-
-- Used to connect bot to PM for managing filters 
-- it helps to avoid spamming in groups.
-
-<b>NOTE:</b>
-1. Only admins can add a connection.
-2. Send <code>/connect</code> for connecting me to ur PM
-
-<b>Commands and Usage:</b>
-• /connect  - <code>connect a particular chat to your PM</code>
-• /disconnect  - <code>disconnect from a chat</code>
-• /connections - <code>list all your connections</code>"""
-    EXTRAMOD_TXT = """Help: <b>Extra Modules</b>
-
-<b>NOTE:</b>
-these are the extra features of Eva Maria
-
-<b>Commands and Usage:</b>
-• /id - <code>get id of a specified user.</code>
-• /info  - <code>get information about a user.</code>
-• /imdb  - <code>get the film information from IMDb source.</code>
-• /search  - <code>get the film information from various sources.</code>"""
-    ADMIN_TXT = """Help: <b>Admin mods</b>
-
-<b>NOTE:</b>
-This module only works for my admins
-
-<b>Commands and Usage:</b>
-• /logs - <code>to get the rescent errors</code>
-• /stats - <code>to get status of files in db.</code>
-• /delete - <code>to delete a specific file from db.</code>
-• /users - <code>to get list of my users and ids.</code>
-• /chats - <code>to get list of the my chats and ids </code>
-• /leave  - <code>to leave from a chat.</code>
-• /disable  -  <code>do disable a chat.</code>
-• /ban  - <code>to ban a user.</code>
-• /unban  - <code>to unban a user.</code>
-• /channel - <code>to get list of total connected channels</code>
-• /broadcast - <code>to broadcast a message to all users</code>"""
     STATUS_TXT = """★ 𝚃𝙾𝚃𝙰𝙻 𝙵𝙸𝙻𝙴𝚂: <code>{}</code>
 ★ 𝚃𝙾𝚃𝙰𝙻 𝚄𝚂𝙴𝚁𝚂: <code>{}</code>
 ★ 𝚃𝙾𝚃𝙰𝙻 𝙲𝙷𝙰𝚃𝚂: <code>{}</code>
@@ -174,6 +29,7 @@ This module only works for my admins
 🖥️ 𝖢𝗁𝖺𝗍𝗌            - <code>{}</code>
 𝖴𝗌𝖾𝖽 𝖲𝗍𝗈𝗋𝖺𝗀𝖾   - <code>{}</code>MB
 𝖥𝗋𝖾𝖾 𝖲𝗍𝗈𝗋𝖺𝗀𝖾    - <code>{}</code>MB"""
+    
     LOG_TEXT_G = """#NewGroup
 Group = {}(<code>{}</code>)
 Total Members = <code>{}</code>
@@ -189,7 +45,6 @@ Name - {}
 
 ╔═══ ᴊᴏɪɴ ᴡɪᴛʜ ᴜs ═══╗ 
 ➲ <a href='t.me/MCUupdatesLINKS'>@OTT UPDTES</a>
-➲ <a href='t.me/+AgeLXhJ_dMpmMmI1'>@PʀᴇDᴠD Lᴀᴛᴇꜱᴛ</a>
 ➲ <a href='https://t.me/+uA5gEKm8WXk1ZTll'>@MAIN CHANNEL</a>
 ╚═══ ᴊᴏɪɴ ᴡɪᴛʜ ᴜs ═══╝
 </b>"""
