e681af87648f85de1da44650f3dd56b92fb98dc5
miltiaboy <114646328+miltiaboy@users.noreply.github.com>
2025-06-26 17:32:59 +0530

    Update info.py

--- info.py
+++ info.py
@@ -39,11 +39,12 @@ REQ_CHANNEL2=environ.get("REQ_CHANNEL2")
 REQ_CHANNEL2 = (int(REQ_CHANNEL2) if REQ_CHANNEL2 and id_pattern.search(REQ_CHANNEL2) else False) if REQ_CHANNEL2 is not None else None
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://saidalimuhamed88:iladias2025@cluster0.qt4dv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-DATABASE_URI2 = environ.get('DATABASE_URI2', "mongodb+srv://saidalimuhamed88:iladias2025@cluster0.qt4dv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-DATABASE_URI3 = environ.get('DATABASE_URI3', "mongodb+srv://saidalimuhamed88:iladias2025@cluster0.qt4dv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-DATABASE_NAME = environ.get('DATABASE_NAME', "Cluster0")
-COLLECTION_NAME = environ.get('COLLECTION_NAME', 'mcu_files')
+USERS_DB = environ.get('USERS_DB', "")
+DATABASE_URI = environ.get('DATABASE_URI', USERS_DB)
+DATABASE_URI2 = environ.get('DATABASE_URI2', USERS_DB)
+DATABASE_URI3 = environ.get('DATABASE_URI3', USERS_DB)
+DATABASE_NAME = environ.get('DATABASE_NAME', "pookie")
+COLLECTION_NAME = environ.get('COLLECTION_NAME', 'mu_files')
 
 #Auto approve
 
