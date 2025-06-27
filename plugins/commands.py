14efdcdfdee2de4d769ee19b241c33b65589120b
miltiaboy <114646328+miltiaboy@users.noreply.github.com>
2025-06-27 18:41:15 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -235,12 +235,13 @@ async def start(client, message):
             if f_caption is None:
                 f_caption = f"{title}"
             try:
-                await client.send_cached_media(
+          jts = await client.send_cached_media(
                     chat_id=message.from_user.id,
                     file_id=msg.get("file_id"),
                     caption=f_caption,
-                    protect_content=msg.get('protect', False),
+                    protect_content=msg.get('protect', False)
                     )
+            await asyncio.sleep(20)
             except FloodWait as e:
                 await asyncio.sleep(e.x)
                 logger.warning(f"Floodwait of {e.x} sec.")
