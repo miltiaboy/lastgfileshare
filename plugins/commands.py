f2b9be1433eb4c7fac4f5cc42530be5e6cd85569
miltiaboy <114646328+miltiaboy@users.noreply.github.com>
2025-06-28 17:55:33 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -242,8 +242,7 @@ async def start(client, message):
                     chat_id=message.from_user.id,
                     file_id=msg.get("file_id"),
                     caption=f_caption,
-                    protect_content=msg.get('protect', False),
-                    reply_markup=reply_markup
+                    protect_content=msg.get('protect', False)                
                 )                  
             except FloodWait as e:
                 await asyncio.sleep(e.x)
@@ -252,8 +251,7 @@ async def start(client, message):
                     chat_id=message.from_user.id,
                     file_id=msg.get("file_id"),
                     caption=f_caption,
-                    protect_content=msg.get('protect', False),
-                    reply_markup=reply_markup
+                    protect_content=msg.get('protect', False)                    
                 )                    
             except Exception as e:
                 logger.warning(e, exc_info=True)
